653c907bd8514aa647bc88829ccc26764e7274c4
takaya <ufkq56586@mineo.jp>
2022-03-14 10:56:56 +0900

    2022_03_14_like-count

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Tweet;
 use App\Models\Comment;
+use App\Models\Like;
 
 class TweetController extends Controller
 {
@@ -22,14 +23,24 @@ class TweetController extends Controller
     public function destroytweet(Request $request){
         Tweet::find($request->id)->delete();
         Comment::where('tweets_id',$request->id)->delete();
+        Like::where('tweets_id',$request->id)->delete();
         return response()->json(['message' => 'Successfully complete destroy']);
     }
     public function countup(Request $request){
-        Tweet::find($request->id)->increment('nice_count');
-        // $item=Tweet::find($request->id);
-        // $item->increment('nice_count');
-        // $item->save();
-        return response()->json(['message' => 'Successfully complete countup']);
+        $likeuser=Like::where('tweets_id',$request->tweets_id)->first();
+        if(is_null($likeuser)||$likeuser->like_name!=$request->like_name){
+            Tweet::find($request->tweets_id)->increment('nice_count');
+            Like::create([
+                "tweets_id"=>$request->tweets_id,
+                "like_name"=>$request->like_name,
+            ]);
+            $mode='inc';
+        }else{
+            Tweet::find($request->tweets_id)->decrement('nice_count');
+            Like::where('tweets_id',$request->tweets_id)->delete();
+            $mode='dec';
+        }
+        return response()->json(['message' => 'Successfully complete countup mode:'.$mode]);
     }
     public function getcount(Request $request){
         return Tweet::find($request->id)->nice_count;
---
+++ database/migrations/2022_03_14_094141_create_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('tweets_id');
+            $table->string('like_name');
+            $table->timestamp('created_at')->useCurrent()->Nullable();
+            $table->timestamp('updated_at')->useCurrent()->Nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
